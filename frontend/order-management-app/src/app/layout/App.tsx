7af5788a66f325f8ebabb405cb76c851e28933cd
Michael Iwanek <miwanek11199@gmail.com>
2023-06-24 21:06:10 -0700

    used codegen with codegen.ts file to automatically create schema.ts and function to pull data from backend.  was able to succesfully retrieve data from c# backend running in localhost from typescript from a component for the first time

---
+++ frontend/order-management-app/src/app/layout/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './styles.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import CustomersDashboard from '../../features/customers/customersDashboard/CustomersDashboard'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache({
+    typePolicies: {}
+  }),
+  uri: "http://localhost:5020/graphql/"
+})
+
+function App() {
+  
+
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <CustomersDashboard />
+      </ApolloProvider>
+    </>
+  )
+}
+
+export default App
