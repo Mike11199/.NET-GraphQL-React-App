e7ecc4b1df0f8bdd727e37a63a3948ad867ed21d
Michael Iwanek <miwanek11199@gmail.com>
2023-06-25 20:55:15 -0700

    added ag-grid for customers data retrieved by graphql codegen generated query to c# backend

---
+++ frontend/order-management-app/src/features/customers/customersDashboard/CustomerList.tsx
@@ -0,0 +1,54 @@
+import { Address, Customer } from "../../../graphql/generated/schema"
+import { useState, useMemo } from "react"
+import { AgGridReact } from "ag-grid-react";
+import { ColDef } from "ag-grid-community";
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+
+interface CustomerListProps {
+    customers: Customer[]
+}
+
+export default function CustomerList({customers}: CustomerListProps) {
+    const [columnDefs] = useState([
+        {
+            field: 'id',
+            width: 50,
+            suppressSizeToFit: true
+        },        
+        {field: 'firstName'},
+        {field: 'lastName'},        
+        {field: 'contactNumber'},        
+        {field: 'email'},        
+        {
+            field: 'address',
+            cellRenderer: function(params: any) {
+                const address = params.value as Address
+                return address.addressLine1
+                    + ', ' + address.addressLine2
+                    + ', ' + address.city
+                    + ', ' + address.state
+                    + ', ' + address.country
+            }    
+        }       
+    ])
+
+    // By wrapping the computation in useMemo, the value of defaultColDef will only be calculated once when the component is initially rendered,
+    // and subsequent re-renders will use the memoized value unless the dependencies of useMemo change.
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true,
+        resizable: true,
+    }), [])
+
+    return (
+        <div className="ag-theme-alpine" style={{ height: 500, width: "100%" }}>
+        <AgGridReact
+          rowData={customers}
+          columnDefs={columnDefs as ColDef[]}
+          defaultColDef={defaultColDef}
+        />
+      </div>
+    )
+
+}
--- frontend/order-management-app/src/features/customers/customersDashboard/CustomersDashboard.tsx
+++ frontend/order-management-app/src/features/customers/customersDashboard/CustomersDashboard.tsx
@@ -1,4 +1,6 @@
-import { useGetCustomersQuery } from '../../../graphql/generated/schema';
+import { Customer, useGetCustomersQuery } from '../../../graphql/generated/schema';
+import { Grid, Typography } from '@mui/material'
+import CustomerList from './CustomerList';
 
 export default function CustomersDashboard() {
 
@@ -11,14 +13,27 @@ export default function CustomersDashboard() {
     if (error || !customersData){
         return <div>Error...</div>
     }
+
+    const customers = customersData.customers as Customer[]
+
     return (
-        <div>
-            <h2>Customers</h2>
-            <ul>
-                {customersData.customers?.map( customer=> (
-                    <li key={customer?.id}>{customer?.firstName}</li>
-                ))}
-            </ul>
-        </div>
+        // <div>
+        //     <h2>Customers</h2>
+        //     <ul>
+        //         {customersData.customers?.map( customer=> (
+        //             <li key={customer?.id}>{customer?.firstName}</li>
+        //         ))}
+        //     </ul>
+        // </div>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography component="div" variant="h5" display="block" gutterBottom align="center">
+                    Customers List
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <CustomerList customers={customers}/>
+            </Grid>
+        </Grid>
     )
 }
